a5a2a4dd973cf39e888cd4690363593e74dfde29
Richard Slater <github@richard-slater.co.uk>
2016-07-27 19:06:26 +0100

    Completed Adapter for mapping languages.

--- src/Markdig.SyntaxHighlighting.Tests/LanguageTypeAdapterTests.cs
+++ src/Markdig.SyntaxHighlighting.Tests/LanguageTypeAdapterTests.cs
@@ -2,13 +2,27 @@
 
 namespace Markdig.SyntaxHighlighting.Tests {
     public class LanguageTypeAdapterTests {
+        private const string AspxCsFirstLine = "<%@ Page Language=\"C#\" %>";
+
         [Theory]
-        [InlineData("csharp", "c#")]
-        [InlineData("css", "css")]
-        public void CanParseCSharp(string inputLanguage, string expectedId) {
+        [InlineData("csharp", "c#", null)]
+        [InlineData("cplusplus", "cpp", null)]
+        [InlineData("css", "css", null)]
+        [InlineData("aspx", "aspx(c#)", AspxCsFirstLine)]
+        [InlineData("javascript", "javascript", "var myVar = 1;")]
+        public void CanParse(string inputLanguage, string expectedId, string firstLine) {
+            var adapter = new LanguageTypeAdapter();
+            var result = adapter.Parse(inputLanguage, firstLine);
+            Assert.Equal(expectedId, result.Id);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("fubar")]
+        public void CanNotParse(string inputLanguage) {
             var adapter = new LanguageTypeAdapter();
             var result = adapter.Parse(inputLanguage);
-            Assert.Equal(expectedId, result.Id);
+            Assert.Null(result);
         }
     }
 }
