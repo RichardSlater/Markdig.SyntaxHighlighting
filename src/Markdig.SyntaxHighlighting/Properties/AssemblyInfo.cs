d60045f285d4185ee330d4873d749c0f901bf921
Jesper Niedermann <jn@uvdata.dk>
2018-10-04 23:41:37 +0200

    Unittests work

---
+++ src/Markdig.SyntaxHighlighting/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Resources;
+
+[assembly: AssemblyTitle("Markdig.SyntaxHighlighting")]
+[assembly: AssemblyDescription("")]
+[assembly: NeutralResourcesLanguage("en")]
