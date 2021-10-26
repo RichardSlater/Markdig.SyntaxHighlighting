cd0dd7790a9dbe2a63fc73bd5c0fc40ffd69a4e1
The Sharp Ninja <ninja@thesharp.ninja>
2021-10-25 23:50:58 -0500

    Updates for .Net 6.0

--- src/Markdig.SyntaxHighlighting.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Markdig.SyntaxHighlighting.Tests")]
-[assembly: AssemblyDescription("")]
-[assembly: Guid("b0696b7b-33c0-49b3-b8b2-de07a7fcfbe9")]
