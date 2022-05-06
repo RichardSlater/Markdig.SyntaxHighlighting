1b31b28a565d98c97bf3d1524e88c5eb31065f1d
The Sharp Ninja <ninja@thesharp.ninja>
2022-05-06 11:37:45 -0500

    Updated Package Handling

--- src/Markdig.SyntaxHighlighting/SyntaxHighlightingExtensions.cs
+++ src/Markdig.SyntaxHighlighting/SyntaxHighlightingExtensions.cs
@@ -2,7 +2,8 @@
 
 namespace Markdig.SyntaxHighlighting {
     public static class SyntaxHighlightingExtensions {
-        public static MarkdownPipelineBuilder UseSyntaxHighlighting(this MarkdownPipelineBuilder pipeline, IStyleSheet customCss = null) {
+        public static MarkdownPipelineBuilder UseSyntaxHighlighting(this MarkdownPipelineBuilder pipeline, IStyleSheet customCss = null)
+        {
             pipeline.Extensions.Add(new SyntaxHighlightingExtension(customCss));
             return pipeline;
         }
